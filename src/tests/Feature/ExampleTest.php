5e4481fe725c49fca766aacc3acf9bca16652b07
Rino Okamoto <rino.okamoto.movie@gmail.com>
2026-03-12 01:42:50 +0900

    ExampleTest.php変更

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -2,14 +2,12 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase; // 追加
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use RefreshDatabase;
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
